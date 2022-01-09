4bdc8e8f96b42fe93063905360ab62592cac0aa7
Francis Langit <francis.villanueva.langit@gmail.com>
2022-01-09 09:43:36 +0800

    Create folder and file for Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Form } from './components/Form/Form';
+
 const App = () => {
-  return <div>Resumake</div>;
+  return (
+    <div>
+      <Form />
+    </div>
+  );
 };
 
 export default App;
