bd2841ed75e4b89ec7c136aff988f029ce5490eb
Francis Langit <francis.villanueva.langit@gmail.com>
2022-01-11 17:19:31 +0800

    Display Name, Email, Number as Field components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import { Education } from './components/Education/Education';
 import { Experience } from './components/Experience/Experience';
 import { Field } from './components/Field/Field';
-import { Form } from './components/Form/Form';
 import { Section } from './components/Section/Section';
 
 const App = () => {
   return (
     <div>
-      <Form
-        fields={[
-          <Field label="Name" />,
-          <Field label="Email" />,
-          <Field label="Number" />,
-        ]}
-      />
+      <Field label="Name" />
+      <Field label="Email" />
+      <Field label="Number" />
       <hr />
-
       <Section form={<Education />} />
       <Section form={<Experience />} />
     </div>
