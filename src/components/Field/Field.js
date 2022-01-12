5c74b9bf9c28ea8ce8216c1c6081086c59f1e1eb
Francis Langit <francis.villanueva.langit@gmail.com>
2022-01-12 14:39:59 +0800

    Allow enter button to submit text in Fields

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,23 +1,33 @@
 import { useState } from 'react';
 
-import { FieldButton } from './FieldButton';
-import { FieldText } from './FieldText';
-
 const Field = ({ placeholder }) => {
   const [isEdit, setIsEdit] = useState(true);
   const [text, setText] = useState('');
 
-  return (
-    <div>
-      <FieldText
-        isEdit={isEdit}
-        text={text}
-        setText={setText}
-        placeholder={placeholder}
-      />
-      <FieldButton isEdit={isEdit} setIsEdit={setIsEdit} />
-    </div>
-  );
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsEdit(!isEdit);
+  };
+
+  if (isEdit) {
+    return (
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input onChange={(e) => handleChange(e)} placeholder={placeholder} />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  } else {
+    return (
+      <div>
+        {text}
+        <button onClick={() => setIsEdit(!isEdit)}>Edit</button>
+      </div>
+    );
+  }
 };
 
 export { Field };
