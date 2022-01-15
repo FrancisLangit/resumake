25deacb38f40e4b6099c3b276fcee9248808e6e8
Francis Langit <francis.villanueva.langit@gmail.com>
2022-01-15 14:49:31 +0800

    Add style to Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+import style from './Form.module.scss';
+
 const Form = ({ fields }) => {
   const [isDelete, setIsDelete] = useState(false);
 
   return isDelete ? null : (
-    <div>
-      {fields.map((f) => (
-        <div key={uuidv4()}>{f}</div>
-      ))}
-      <button onClick={() => setIsDelete(!isDelete)}>Delete</button>
+    <div className={style.container}>
+      <div className={style.fields}>
+        {fields.map((f) => (
+          <div key={uuidv4()}>{f}</div>
+        ))}
+      </div>
+      <button onClick={() => setIsDelete(!isDelete)} className={style.delete}>
+        Delete
+      </button>
     </div>
   );
 };
