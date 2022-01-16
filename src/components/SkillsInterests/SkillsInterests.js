025ea9a10596e38023c70b8e02e32c53ae9c1d4f
Francis Langit <francis.villanueva.langit@gmail.com>
2022-01-16 21:04:41 +0800

    Add styling for SkillsInterests

--- src/components/SkillsInterests/SkillsInterests.js
+++ src/components/SkillsInterests/SkillsInterests.js
@@ -1,11 +1,19 @@
 import { Field } from '../Field/Field';
 
+import style from './SkillsInterests.module.scss';
+
 const SkillsInterests = () => {
   return (
-    <div>
-      <Field name="Skills" isTextarea={true} />
-      <Field name="Interests" isTextarea={true} />
-    </div>
+    <>
+      <div className={style.wrapper}>
+        <div className={style.header}>Skills</div>
+        <Field name="What are you good at?" isTextarea={true} />
+      </div>
+      <div className={style.wrapper}>
+        <div className={style.header}>Interests</div>
+        <Field name="What are your hobbies?" isTextarea={true} />
+      </div>
+    </>
   );
 };
 
